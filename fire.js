8af77a2aeafd0ba7f98129d5dc0687a0472ec6a0
ThazSobral <thazsobral@gmail.com>
2019-09-22 08:10:18 -0300

    add function createdDataStructure

--- fire.js
+++ fire.js
@@ -1,8 +1,17 @@
+const firePixelsArray = []
+const fireWidth = 2, fireHeight = 2
+
 function start() {
+  createdFireDataStructure()
+  renderFire()
 }
 
 function createdFireDataStructure () {
-  
+  const numberOfPixels = fireHeight * fireWidth
+
+  for (let i = 0; i < numberOfPixels; i++) {
+    firePixelsArray[i] = 0
+  }
 }
 
 function calculateFirePropagation () {
@@ -10,6 +19,6 @@ function calculateFirePropagation () {
 }
 
 function renderFire () {
-  
+
 }
 start()
